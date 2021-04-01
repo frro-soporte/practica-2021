d30d1777c6c035046f6c63fbcee761779c7f6e19
Juan Diego Ruiz Di Paolo <juandiego1064@gmail.com>
2021-04-01 01:10:28 -0300

    P1 E08

--- practico_01/ejercicio_08.py
+++ practico_01/ejercicio_08.py
@@ -9,8 +9,11 @@ def superposicion_basico(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool
 
     Restricción: Utilizar bucles anidados.
     """
-    pass # Completar
-
+    for i in range(0,len(lista_1)):
+        for j in range(0,len(lista_2)):
+            if lista_1[i] == lista_2[j]:
+                return True
+            
 
 # NO MODIFICAR - INICIO
 test_list = [1, "hello", 35.20]
@@ -24,7 +27,10 @@ assert not superposicion_basico(test_list, (2, "world", 30.85))
 
 def superposicion_in(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
     """Re-Escribir utilizando un sólo bucle y el operador IN."""
-    pass # Completar
+    for i in range(0,len(lista_1)):
+        if lista_1[i] in lista_2:
+            return True
+
 
 
 # NO MODIFICAR - INICIO
@@ -41,7 +47,7 @@ def superposicion_any(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
     """Re-Escribir utilizando sin bucles, el operador in y la funcion any.
     Referencia: https://docs.python.org/3/library/functions.html#any
     """
-    pass # Completar
+    return any(map(lambda v: v in lista_2, lista_1))
 
 
 # NO MODIFICAR - INICIO
@@ -58,8 +64,13 @@ def superposicion_set(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
     """Re-Escribir utilizando conjuntos (sets).
     Referencia: https://docs.python.org/3/library/stdtypes.html#set-types-set-frozenset
     """
-    pass # Completar
-
+    lista_1 = set(lista_1)
+    lista_2 = set(lista_2)
+    interseccion = lista_1 & lista_2
+    if len(interseccion) != 0:
+        return True
+    else:
+        return False
 
 # NO MODIFICAR - INICIO
 test_list = [1, "hello", 35.20]
