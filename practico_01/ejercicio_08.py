ced9c6d5ca0a86d5c6367b185a2b16eb6ee4862b
Matias <santolarimatias@gmail.com>
2021-04-15 14:14:57 -0300

    Resolucion Ejercicio 08(Enunciado 1 y 2)

--- practico_01/ejercicio_08.py
+++ practico_01/ejercicio_08.py
@@ -9,7 +9,12 @@ def superposicion_basico(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool
 
     Restricción: Utilizar bucles anidados.
     """
-    pass # Completar
+    for i in lista_1:
+        for j in lista_2:
+            if i==j:
+                return True
+                break
+
 
 
 # NO MODIFICAR - INICIO
@@ -24,7 +29,11 @@ assert not superposicion_basico(test_list, (2, "world", 30.85))
 
 def superposicion_in(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
     """Re-Escribir utilizando un sólo bucle y el operador IN."""
-    pass # Completar
+
+    for i in lista_1:
+        if i in lista_2:
+            return True
+            break
 
 
 # NO MODIFICAR - INICIO
