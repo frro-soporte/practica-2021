726af71af7ef704035503fbdb8d80259c9b4f27a
Matias <santolarimatias@gmail.com>
2021-04-15 15:12:56 -0300

    Resolucion Ejercicio 08

--- practico_01/ejercicio_08.py
+++ practico_01/ejercicio_08.py
@@ -13,7 +13,6 @@ def superposicion_basico(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool
         for j in lista_2:
             if i==j:
                 return True
-                break
 
 
 
@@ -33,7 +32,7 @@ def superposicion_in(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
     for i in lista_1:
         if i in lista_2:
             return True
-            break
+
 
 
 # NO MODIFICAR - INICIO
@@ -50,7 +49,8 @@ def superposicion_any(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
     """Re-Escribir utilizando sin bucles, el operador in y la funcion any.
     Referencia: https://docs.python.org/3/library/functions.html#any
     """
-    pass # Completar
+    return any([i for i in list(lista_1) + list(lista_2) if i in lista_1 and i in lista_2])
+
 
 
 # NO MODIFICAR - INICIO
@@ -67,7 +67,7 @@ def superposicion_set(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
     """Re-Escribir utilizando conjuntos (sets).
     Referencia: https://docs.python.org/3/library/stdtypes.html#set-types-set-frozenset
     """
-    pass # Completar
+    return any(list(set(lista_1).intersection(set(lista_2))))
 
 
 # NO MODIFICAR - INICIO
