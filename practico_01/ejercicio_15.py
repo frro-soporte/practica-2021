6b9c196beb99cb7a6c6dd41e9982830d5d04d74e
Juan Diego Ruiz Di Paolo <juandiego1064@gmail.com>
2021-04-25 22:29:37 -0300

    Estilos

--- practico_01/ejercicio_15.py
+++ practico_01/ejercicio_15.py
@@ -191,7 +191,7 @@ sucesivas.
 @memoized
 def calcular_posibilidades_recursiva(lista: Sequence[int], limite: int) -> int:
     """Re-Escribir de manera recursiva"""
-    pass # Completar
+    pass  # Completar
 
 
 # NO MODIFICAR - INICIO
