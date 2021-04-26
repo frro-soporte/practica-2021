4fedbb7b89b9ff58c456658156d477797444919c
Ivan Biscaldi <ivanbisc12@gmail.com>
2021-04-26 12:39:58 -0300

    Ejercicio 15 primera parte

--- practico_01/ejercicio_15.py
+++ practico_01/ejercicio_15.py
@@ -55,7 +55,10 @@ def medir_tiempo(func: Callable[[], int]) -> Tuple[int, float]:
     Restricción: La función no debe tomar parámetros y por lo tanto se
     recomienda usar partial.
     """
-    pass # Completar
+    start= perf_counter()
+    resultado=func()
+    stop=perf_counter()-start
+    return resultado, stop
 
 
 # NO MODIFICAR - INICIO
