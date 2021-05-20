13887fdcda299f7ed131aca892858d67acc5bdf5
Ivan Biscaldi <ivanbisc12@gmail.com>
2021-05-20 20:40:43 -0300

    ejercicio_03

--- practico_03/ejercicio_03.py
+++ practico_03/ejercicio_03.py
@@ -1,6 +1,9 @@
 """Dataclasses"""
 
 
+from dataclasses import dataclass
+
+
 class Persona:
     """Clase con los siguientes miembros:
 
@@ -15,7 +18,16 @@ class Persona:
     - es_mayor_edad(): indica si es mayor de edad, devuelve un booleano.
     """
 
-    # Completar
+    def __init__(self, nombre: str = None, edad: int = None, sexo: str = None,
+                 peso: float = None, altura: float = None) -> None:
+        self.nombre = nombre
+        self.edad = edad
+        self.sexo = sexo
+        self.peso = peso
+        self.altura = altura
+
+    def es_mayor_edad(self) -> bool:
+        return self.edad >= 18
 
 
 # NO MODIFICAR - INICIO
@@ -27,13 +39,17 @@ assert not Persona("Julia", 16, "M", 65, 162.4).es_mayor_edad()
 ###############################################################################
 
 
-from dataclasses import dataclass
-
 @dataclass
 class Persona:
     """Re-Escribir utilizando DataClasses"""
+    nombre: str
+    edad: int
+    sexo: str
+    peso: float
+    altura: float
 
-    # Completar
+    def es_mayor_edad(self) -> bool:
+        return self.edad >= 18
 
 
 # NO MODIFICAR - INICIO
