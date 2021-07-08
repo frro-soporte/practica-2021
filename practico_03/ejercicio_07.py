3b3803ad00047daa34c926ee659bd53843479cce
Joan Romero <joanromerosfc@gmail.com>
2021-07-08 14:22:10 -0300

    ejercicio_07

--- practico_03/ejercicio_07.py
+++ practico_03/ejercicio_07.py
@@ -23,8 +23,11 @@ def actualizar_precio(articulos: List[Articulo], porcentaje: float) -> List[Arti
     Restricción: NO se debe modificar la clase ni los tests.
     Hint: Usar deepcopy (https://docs.python.org/3/library/copy.html#copy.deepcopy)
     """
-    deepcopy(articulos)
-
+    art_nuevos = []
+    for articulo in deepcopy(articulos):
+        articulo.precio *= 1 + porcentaje / 100
+        art_nuevos.append(articulo)
+    return art_nuevos
 
 # NO MODIFICAR - INICIO
 nombres = ["sabana", "parlante", "computadora", "tasa", "botella", "celular"]
