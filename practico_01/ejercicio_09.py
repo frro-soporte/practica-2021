26079eaf578b4757f1fd39dccc2961ee74efd2ca
Joaquin-Garola <joaquingarola75@gmail.com>
2022-03-29 18:06:56 -0300

    Agregando metodos ej09

--- practico_01/ejercicio_09.py
+++ practico_01/ejercicio_09.py
@@ -39,3 +39,37 @@ assert sumatoria_sum(100) == 5050
 ###############################################################################
 
 
+from functools import reduce
+
+
+def sumatoria_reduce(n: int) -> int:
+    """CHALLENGE OPCIONAL: Re-escribir utilizando reduce.
+    Referencia: https://docs.python.org/3/library/functools.html#functools.reduce
+    """
+    pass # Completar
+
+
+# NO MODIFICAR - INICIO
+if __name__ == "__main__":
+    assert sumatoria_reduce(1) == 1
+    assert sumatoria_reduce(100) == 5050
+# NO MODIFICAR - FIN
+
+
+###############################################################################
+
+
+def sumatoria_gauss(n: int) -> int:
+    """CHALLENGE OPCIONAL: Re-Escribir utilizando suma de Gauss.
+    Referencia: https://es.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
+    """
+    pass # Completar
+
+
+# NO MODIFICAR - INICIO
+if __name__ == "__main__":
+    assert sumatoria_gauss(1) == 1
+    assert sumatoria_gauss(100) == 5050
+# NO MODIFICAR - FIN
+
+
