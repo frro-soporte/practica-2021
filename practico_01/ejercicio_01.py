8c04e18af13f746ea91b99a34fa065e40be53ff1
pablo-nardi <nardipablo31@gmail.com>
2022-04-19 20:47:31 -0300

    WIP_3

--- practico_01/ejercicio_01.py
+++ practico_01/ejercicio_01.py
@@ -5,25 +5,11 @@ def maximo_basico(a: float, b: float) -> float:
     valor = a if a > b else b
     return valor
 
-
-
+# print(maximo_basico(4,5))
 
 # NO MODIFICAR - INICIO
 assert maximo_basico(10, 5) == 10
 assert maximo_basico(9, 18) == 18
 # NO MODIFICAR - FIN
-
-
-"""Bloque IF, operadores lógicos, función max y operador ternario."""
-
-
-def maximo_libreria(a: float, b: float) -> float:
-    return max(a, b)
-
-
-
-
-# NO MODIFICAR - INICIO
-assert maximo_libreria(10, 5) == 10
-assert maximo_libreria(9, 18) == 18
-# NO MODIFICAR - FIN
+ 
+ 
--- practico_01/ejercicio_11.py
+++ practico_01/ejercicio_11.py
@@ -32,6 +32,9 @@ assert suma_cubo_pares_for([1, 2, 3, 4, 5, 6]) == 288
 
 ###############################################################################
 
+from typing import Iterable
+from functools import reduce
+
 
 def suma_cubo_pares_sum_list(numeros: Iterable[int]) -> int:
     """Re-Escribir utilizando comprension de listas (debe resolverse en 1 línea)
@@ -41,27 +44,41 @@ def suma_cubo_pares_sum_list(numeros: Iterable[int]) -> int:
     Referencia: https://docs.python.org/3/library/functions.html#sum
     """
     pass # Completar
+    # return reduce(lambda x, y: x + y, filter(lambda x: x % 2 == 0, map(lambda x: x**3, numeros)))
+    return sum([x**3 for x in numeros if x % 2 == 0])
 
 
 # NO MODIFICAR - INICIO
 assert suma_cubo_pares_sum_list([1, 2, 3, 4, 5, 6]) == 288
 # NO MODIFICAR - FIN
+# print(suma_cubo_pares_sum_list([1, 2, 3, 4, 5, 6]))
+
 
 
 ###############################################################################
 
 
-def suma_cubo_pares_sum_gen(numeros: Iterable[int]) -> int:
-    """ Re-Escribir utilizando expresiones generadoras (debe resolverse en 1 línea)
-    y la función sum.
-    Referencia: https://docs.python.org/3/reference/expressions.html#generator-expressions
+from typing import Iterable
+from functools import reduce
+
+
+def suma_cubo_pares_sum_list(numeros: Iterable[int]) -> int:
+    """Re-Escribir utilizando comprension de listas (debe resolverse en 1 línea)
+    y la función built-in sum.
+
+    Referencia: https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
+    Referencia: https://docs.python.org/3/library/functions.html#sum
     """
     pass # Completar
-
+    # return reduce(lambda x, y: x + y, filter(lambda x: x % 2 == 0, map(lambda x: x**3, numeros)))
+    # return sum([x**3 for x in numeros if x % 2 == 0])
+    return sum((x**3 for x in numeros if x % 2 == 0))
 
 # NO MODIFICAR - INICIO
-assert suma_cubo_pares_sum_gen([1, 2, 3, 4, 5, 6]) == 288
+assert suma_cubo_pares_sum_list([1, 2, 3, 4, 5, 6]) == 288
 # NO MODIFICAR - FIN
+# print(suma_cubo_pares_sum_list([1, 2, 3, 4, 5, 6]))
+
 
 
 ###############################################################################
@@ -76,25 +93,27 @@ numeros = [1, 2, 3, 4, 5, 6]
 
 # Escribir una función lambda que eleve los elementos al cubo
 
-numeros_al_cubo = # Completar
+
+numeros_al_cubo = map(lambda x, y: x ** 3, numeros)
 
 
 # Escribir una función lambda que permita filtrar todos los elementos pares
 
-numeros_al_cubo_pares = # Completar
+numeros_al_cubo_pares = filter(lambda x, y: x % 2 == 0, numeros)
 
 
 # Escribir una función Lambda que sume todos los elementos
 
 from functools import reduce
 
-suma_numeros_al_cubo_pares = # Completar
+suma_numeros_al_cubo_pares = reduce(lambda x, y: x + y, numeros)
+
 
 
 # Escribir una función Lambda que permita ordenar los elementos de la numeros
 # en base a si son pares o impares
 
-numeros_ordenada = # Completar
+# numeros_ordenada = # Completar
 
 # NO MODIFICAR - INICIO
 assert numeros_al_cubo == [1, 8, 27, 64, 125, 216]
@@ -102,3 +121,4 @@ assert numeros_al_cubo_pares == [8, 64, 216]
 assert suma_numeros_al_cubo_pares == 288
 assert numeros_ordenada == [1, 3, 5, 2, 4, 6]
 # NO MODIFICAR - FIN
+
