d99479eac508a07024d351cd8ac143ed3fab9f8a
Colo07 <fergardenes7@gmail.com>
2022-04-19 00:42:34 -0300

    Ejercicio 13 completo

--- practico_01/ejercicio_13.py
+++ practico_01/ejercicio_13.py
@@ -21,7 +21,15 @@ def generar_pares_clousure(initial: int = 0) -> Callable[[], int]:
         - Usar closures
         - Usar el modificador nonlocal
     """
-    pass # Completar
+    def siguientePar():
+            nonlocal initial
+            if initial == 0:
+                initial +=2
+                return 0       
+            if initial > 0:
+                initial +=2
+                return initial-2            
+            return siguientePar
 
 
 # NO MODIFICAR - INICIO
@@ -45,7 +53,16 @@ def generar_pares_generator(initial: int = 0) -> Iterator[int]:
     """Re-Escribir utilizando Generadores
     Referencia: https://docs.python.org/3/howto/functional.html?highlight=generator#generators
     """
-    pass # Completar
+    for i in range (999):
+        if initial == 0:
+            yield initial
+            initial+=2
+
+        if initial >0:
+            yield initial
+            initial +=2
+        
+
 
 
 # NO MODIFICAR - INICIO
