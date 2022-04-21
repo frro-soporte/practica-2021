b557ab96263a5f50146b711f4ba4add1d0c5f3e9
unknown <fernandoo@gmail.com>
2022-04-21 09:47:40 -0300

    Ejercicio 6

--- practico_01/ejercicio_06.py
+++ practico_01/ejercicio_06.py
@@ -2,12 +2,25 @@
 
 from typing import List, Union
 
+from numpy.core.defchararray import isnumeric
+
 
 def numeros_al_final_basico(lista: List[Union[float, str]]) -> List[Union[float, str]]:
     """Toma una lista de enteros y strings y devuelve una lista con todos los
     elementos numéricos al final.
     """
     pass # Completar
+    numberList =[]
+    charList = []
+
+    for i in range(len(lista)):
+        if str(lista[i]).isdigit():
+            numberList.append(lista[i])
+        else:
+            charList.append(lista[i])
+
+    return charList+numberList
+
 
 
 # NO MODIFICAR - INICIO
@@ -21,6 +34,13 @@ assert numeros_al_final_basico([3, "a", 1, "b", 10, "j"]) == ["a", "b", "j", 3, 
 def numeros_al_final_comprension(lista: List[Union[float, str]]) -> List[Union[float, str]]:
     """Re-escribir utilizando comprensión de listas."""
     pass # Completar
+
+    numberList = [element for element in lista if str(element).isdigit()]
+    charList = [element for element in lista if not(str(element).isdigit())]
+
+    return charList + numberList
+
+
 
 
 # NO MODIFICAR - INICIO
@@ -37,6 +57,7 @@ def numeros_al_final_sorted(lista: List[Union[float, str]]) -> List[Union[float,
     """
     pass # Completar
 
+    return sorted(lista, key=lambda element: str(element).isdigit())
 
 # NO MODIFICAR - INICIO
 assert numeros_al_final_sorted([3, "a", 1, "b", 10, "j"]) == ["a", "b", "j", 3, 1, 10]
@@ -52,6 +73,10 @@ def numeros_al_final_filter(lista: List[Union[float, str]]) -> List[Union[float,
     """
     pass # Completar
 
+    numberList = list(filter(lambda element: str(element).isdigit(), lista))
+    charList = list(filter(lambda element: not(str(element).isdigit()), lista))
+
+    return charList + numberList
 
 # NO MODIFICAR - INICIO
 if __name__ == "__main__":
@@ -65,7 +90,8 @@ if __name__ == "__main__":
 def numeros_al_final_recursivo(lista: List[Union[float, str]]) -> List[Union[float, str]]:
     """CHALLENGE OPCIONAL - Re-escribir de forma recursiva."""
     pass # Completar
-
+    "Una función recursiva debe llamarse a sí misma, falta realizar"
+    return numeros_al_final_sorted(lista)
 
 # NO MODIFICAR - INICIO
 if __name__ == "__main__":
