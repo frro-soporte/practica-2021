c20797e26d8a78a051c002ee5599ba1cbfdbeb75
brengodoy <dia.godoybrenda@gmail.com>
2022-04-28 18:23:56 -0300

    ejercicio 03 practica 03

--- practico_03/ejercicio_03.py
+++ practico_03/ejercicio_03.py
@@ -14,7 +14,15 @@ class Persona:
     Métodos:
     - es_mayor_edad(): indica si es mayor de edad, devuelve un booleano.
     """
+    def __init__(self, nombre, edad, sexo, peso, altura):
+        self.nombre = nombre
+        self.edad = edad
+        self.sexo = sexo
+        self.peso = peso
+        self.altura = altura
 
+    def es_mayor_edad(self):
+        return self.edad >= 18
     # Completar
 
 
@@ -32,7 +40,14 @@ from dataclasses import dataclass
 @dataclass
 class Persona:
     """Re-Escribir utilizando DataClasses"""
+    nombre : str
+    edad : int
+    sexo : str
+    peso : float
+    altura : float
 
+    def es_mayor_edad(self):
+        return self.edad >= 18
     # Completar
 
 
