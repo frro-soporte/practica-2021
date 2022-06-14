b86d7d53b316eabe3a1c8381e4a2c1c3e091909b
Julia Arrospidegaray <julia.arrospidegaray@syloper.com>
2022-06-14 16:32:49 -0300

    ejercicio 2

--- practico_03/ejercicio_02.py
+++ practico_03/ejercicio_02.py
@@ -11,6 +11,16 @@ class Articulo:
     """
 
     # Completar
+    _last_id: int = 0
+    
+    def __init__(self, nombre: str = "") -> None:
+        self.nombre: str = nombre
+        self.id_: int = self._get_next_id()
+        
+    @classmethod
+    def _get_next_id(cls):
+        cls._last_id += 1
+        return cls._last_id
 
 
 # NO MODIFICAR - INICIO
