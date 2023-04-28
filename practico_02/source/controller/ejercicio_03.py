85dbfa1a2b77816a2c07cdf6687e821c3217f8f5
quchallenge2021 <quchallenge2021@gmail.com>
2023-04-28 13:20:51 -0300

    Upload tp 2

--- practico_02/source/controller/ejercicio_03.py
+++ practico_02/source/controller/ejercicio_03.py
@@ -12,6 +12,13 @@ $PATH$/practico_02> python -m source.controller.ejercicio_03
 """
 
 # Completar
+import source.controller.controller as controller
+import source.util as util
+import source.data.database as database
+import source.main as main
+
+import config.ci_cd.deploy_travis as deploy_travis
+import config.test_config as test_config
 
 # NO MODIFICAR - INICIO
 assert main.name == "main"
