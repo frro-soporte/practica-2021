19334c5972e31966dbd74e31beeb2acc67f35d22
Matiaja <102177224+Matiaja@users.noreply.github.com>
2023-05-27 19:51:16 -0300

    ejercicio_06.py

--- practico_04/ejercicio_06.py
+++ practico_04/ejercicio_06.py
@@ -1,18 +1,45 @@
-"""Base de Datos SQL - Creación de tablas auxiliares"""
-
-from practico_04.ejercicio_01 import borrar_tabla, crear_tabla
-
-
 def crear_tabla_peso():
+    conexion = sqlite3.connect('mi_base_de_datos.db')
+    cursor = conexion.cursor()
+
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='PersonaPeso'")
+    tabla_existente = cursor.fetchone()
+
+    if tabla_existente is not None:
+        print("La tabla ya existe")
+    else:
+        consulta_sql = '''CREATE TABLE PersonaPeso (
+            IdPersona INTEGER,
+            Fecha TEXT,
+            Peso INTEGER,
+            FOREIGN KEY (IdPersona) REFERENCES persona(IdPersona)
+        )'''
+
+        cursor.execute(consulta_sql)
+
+        conexion.commit()
+        conexion.close()
     """Implementar la funcion crear_tabla_peso, que cree una tabla PersonaPeso con:
         - IdPersona: Int() (Clave Foranea Persona)
         - Fecha: Date()
         - Peso: Int()
     """
+
     pass # Completar
 
 
 def borrar_tabla_peso():
+    conexion = sqlite3.connect('mi_base_de_datos.db')
+    cursor = conexion.cursor()
+
+    comando_sql = '''DROP TABLE IF EXISTS PersonaPeso'''
+
+    cursor.execute(comando_sql)
+
+    conexion.commit()
+    conexion.close()
+
+
     """Implementar la funcion borrar_tabla, que borra la tabla creada 
     anteriormente."""
     pass # Completar
