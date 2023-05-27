8f1538330d58b03be3478d18640b416b71a987c2
Matiaja <102177224+Matiaja@users.noreply.github.com>
2023-05-27 19:50:12 -0300

    ejercicio_04.py

--- practico_04/ejercicio_04.py
+++ practico_04/ejercicio_04.py
@@ -7,6 +7,21 @@ from practico_04.ejercicio_02 import agregar_persona
 
 
 def buscar_persona(id_persona):
+
+    conexion = sqlite3.connect('mi_base_de_datos.db')
+    cursor = conexion.cursor()
+
+    consulta_sql = ''' SELECT IdPersona, Nombre, FechaNacimiento, DNI, Altura FROM persona WHERE IdPersona = ?'''
+    cursor.execute(consulta_sql, (id_persona,))
+    resultado = cursor.fetchone()
+
+    conexion.close()
+
+    if resultado:
+        return resultado
+    else:
+        return False
+
     """Implementar la funcion buscar_persona, que devuelve el registro de una 
     persona basado en su id. El return es una tupla que contiene sus campos: 
     id, nombre, nacimiento, dni y altura. Si no encuentra ningun registro, 
@@ -17,8 +32,8 @@ def buscar_persona(id_persona):
 # NO MODIFICAR - INICIO
 @reset_tabla
 def pruebas():
-    juan = buscar_persona(agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180))
-    assert juan == (1, 'juan perez', datetime.datetime(1988, 5, 15), 32165498, 180)
+    juan = buscar_persona(agregar_persona('juan perez', '1988-05-15', 32165498, 180))
+    assert juan == (1, 'juan perez', '1988-05-15', 32165498, 180)
     assert buscar_persona(12345) is False
 
 if __name__ == '__main__':
